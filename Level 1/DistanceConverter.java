e53fa1ec75d31c08936c63bc9df5fad9bf562ee2
Shivam <sshivam.mpi0123@gmail.com>
2025-06-12 11:19:53 +0530

    Completed Todays Assigment

---
+++ Level 1/DistanceConverter.java
@@ -0,0 +1,13 @@
+
+public class DistanceConverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double Dis=10.8;
+		double OneMile=1.6;
+		double DistanceInMile=Dis*OneMile;
+		System.out.print("The distance "+10.8+" km in miles is "+DistanceInMile);
+		
+	}
+
+}
